cdbd2fbdba459bf609199fda0a7f13309991d0d2
Jeffrey Bertoen <jeffrey@synetic.nl>
2022-01-28 12:30:47 +0100

    Issue #3257863: Separate default_langcode from system site settings

--- domain_config_ui/src/Config/ConfigFactory.php
+++ domain_config_ui/src/Config/ConfigFactory.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\domain_config_ui\Config;
 
-use Drupal\Core\Config\ConfigFactory as CoreConfigFactory;
+use Drupal\Core\Config\ConfigFactory as DomainConfigFactory;
 use Drupal\Core\Config\StorageInterface;
 use Drupal\Core\Config\TypedConfigManagerInterface;
 use Drupal\domain_config_ui\DomainConfigUIManager;
@@ -11,7 +11,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 /**
  * Extends core ConfigFactory class to save domain specific configuration.
  */
-class ConfigFactory extends CoreConfigFactory {
+class ConfigFactory extends DomainConfigFactory {
 
   /**
    * The Domain config UI manager.
