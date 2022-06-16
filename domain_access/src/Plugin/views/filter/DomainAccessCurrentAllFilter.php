90f228189afdf7433b90fd2549e81e272a443b88
Ken Rickard <agentrickard@gmail.com>
2022-06-16 11:06:11 -0400

    Cleans up DomainAccessCurrentAllFilter

--- domain_access/src/Plugin/views/filter/DomainAccessCurrentAllFilter.php
+++ domain_access/src/Plugin/views/filter/DomainAccessCurrentAllFilter.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\domain_access\Plugin\views\filter;
 
+use Drupal\domain\DomainNegotiatorInterface;
 use Drupal\views\Plugin\views\filter\BooleanOperator;
 use Drupal\views\Plugin\views\display\DisplayPluginBase;
 use Drupal\views\ViewExecutable;
@@ -14,6 +15,24 @@ use Drupal\views\ViewExecutable;
  * @ViewsFilter("domain_access_current_all_filter")
  */
 class DomainAccessCurrentAllFilter extends BooleanOperator {
+
+  /**
+   * The label value for the plugin.
+   *
+   * This should be defined by the parent but is not.
+   *
+   * @var string
+   */
+  public $value_value;
+
+  /**
+   * The options provided by the plugin.
+   *
+   * This should be defined by the parent but is not.
+   *
+   * @var array
+   */
+  public $valueOptions;
 
   /**
    * {@inheritdoc}
@@ -42,28 +61,35 @@ class DomainAccessCurrentAllFilter extends BooleanOperator {
    */
   public function query() {
     $this->ensureMyTable();
-    $all_table = $this->query->addTable('node__field_domain_all_affiliates', $this->relationship);
-    $all_field = $all_table . '.field_domain_all_affiliates_value';
-    $real_field = $this->tableAlias . '.' . $this->realField;
-    /** @var DomainNegotiatorInterface $domain_negotiator */
-    $domain_negotiator = \Drupal::service('domain.negotiator');
-    $current_domain = $domain_negotiator->getActiveDomain();
-    $current_domain_id = $current_domain->id();
-    if (empty($this->value)) {
-      $where = "(($real_field <> '$current_domain_id' OR $real_field IS NULL) AND ($all_field = 0 OR $all_field IS NULL))";
-      if ($current_domain->isDefault()) {
-        $where = "($real_field <> '$current_domain_id' AND ($all_field = 0 OR $all_field IS NULL))";
+    if (method_exists($this->query, 'addTable')) {
+      $all_table = $this->query->addTable('node__field_domain_all_affiliates', $this->relationship);
+      $all_field = $all_table . '.field_domain_all_affiliates_value';
+      $real_field = $this->tableAlias . '.' . $this->realField;
+
+      /** @var DomainNegotiatorInterface $domain_negotiator */
+      $domain_negotiator = \Drupal::service('domain.negotiator');
+      $current_domain = $domain_negotiator->getActiveDomain();
+      $current_domain_id = $current_domain->id();
+
+      if (empty($this->value)) {
+        $where = "(($real_field <> '$current_domain_id' OR $real_field IS NULL) AND ($all_field = 0 OR $all_field IS NULL))";
+        if ($current_domain->isDefault()) {
+          $where = "($real_field <> '$current_domain_id' AND ($all_field = 0 OR $all_field IS NULL))";
+        }
       }
-    }
-    else {
-      $where = "($real_field = '$current_domain_id' OR $all_field = 1)";
-      if ($current_domain->isDefault()) {
-        $where = "(($real_field = '$current_domain_id' OR $real_field IS NULL) OR $all_field = 1)";
+      else {
+        $where = "($real_field = '$current_domain_id' OR $all_field = 1)";
+        if ($current_domain->isDefault()) {
+          $where = "(($real_field = '$current_domain_id' OR $real_field IS NULL) OR $all_field = 1)";
+        }
       }
+
+      if (method_exists($this->query, 'addWhereExpression')) {
+        $this->query->addWhereExpression($this->options['group'], $where);
+      }
+      // This filter causes duplicates.
+      $this->query->options['distinct'] = TRUE;
     }
-    $this->query->addWhereExpression($this->options['group'], $where);
-    // This filter causes duplicates.
-    $this->query->options['distinct'] = TRUE;
   }
 
   /**
