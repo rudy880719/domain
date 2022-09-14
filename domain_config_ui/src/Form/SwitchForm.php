2d71215b784c51587a07129af5f9107e409393fe
Jernej Mramor <jernej.mramor@agiledrop.com>
2022-09-14 10:58:10 +0200

    Resolve deprecation issue in domain_config_ui SwitchForm.

--- domain_config_ui/src/Form/SwitchForm.php
+++ domain_config_ui/src/Form/SwitchForm.php
@@ -9,6 +9,7 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\EventSubscriber\MainContentViewSubscriber;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Http\RequestStack;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\domain\DomainInterface;
 use Drupal\domain\DomainElementManagerInterface;
@@ -92,7 +93,8 @@ class SwitchForm extends FormBase {
       $container->get('entity_type.manager'),
       $container->get('language_manager'),
       $container->get('domain_config_ui.manager'),
-      $container->get('domain.element_manager')
+      $container->get('domain.element_manager'),
+      $container->get('request_stack'),
     );
   }
 
@@ -211,7 +213,7 @@ class SwitchForm extends FormBase {
   public static function switchCallback(array &$form, FormStateInterface $form_state) {
     // Extract requesting page URI from ajax URI.
     // Copied from Drupal\Core\Form\FormBuilder::buildFormAction().
-    $request = \Drupal::service('request_stack')->getMasterRequest();
+    $request = \Drupal::service('request_stack')->getMainRequest();
     $request_uri = $request->getRequestUri();
 
     // Prevent cross site requests via the Form API by using an absolute URL
