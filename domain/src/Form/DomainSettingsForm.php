c5e7cb402c98bed3fbf8f67f148750b99d307c72
tobiasb <tobias.baehr@gzevd.de>
2024-02-17 14:45:31 +0100

    Upper case of module name

--- domain/src/Form/DomainSettingsForm.php
+++ domain/src/Form/DomainSettingsForm.php
@@ -6,7 +6,7 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Provides the settings form for domain module.
+ * Provides the settings form for the Domain module.
  *
  * @package Drupal\domain\Form
  */
