810b27de572a1c50d2a18ba93c485615d95e0f81
Ihor Mashevskyi <i.mashevskyi@dev-branch.com>
2024-04-29 10:57:00 +0300

    Issue #3220689 by Igor Mashevskyi: Link to set a theme as default for a domain is missing domain_config_ui_domain querystring

---
+++ domain_config_ui/tests/src/FunctionalJavascript/DomainConfigUIDefaultThemeSettingsTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\Tests\domain_config_ui\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+use Drupal\Tests\domain\Traits\DomainTestTrait;
+use Drupal\Tests\domain_config_ui\Traits\DomainConfigUITestTrait;
+
+/**
+ * Tests the domain config user interface for appearance configuration.
+ *
+ * @group domain_config_ui
+ */
+class DomainConfigUIDefaultThemeSettingsTest extends WebDriverTestBase {
+
+  use DomainTestTrait;
+  use DomainConfigUITestTrait;
+
+  /**
+   * Disabled config schema checking.
+   *
+   * Domain Config actually duplicates schemas provided by other modules,
+   * so it cannot define its own.
+   *
+   * @var bool
+   */
+  protected $strictConfigSchema = FALSE; // phpcs:ignore
+
+  /**
+   * The default theme.
+   *
+   * @var string
+   */
+  protected $defaultTheme = 'olivero';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'domain_config_ui',
+    'domain_config_test',
+    'language',
+  ];
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->createAdminUser();
+
+    $this->setBaseHostname();
+    $this->domainCreateTestDomains(5);
+
+    $this->createLanguage();
+  }
+
+  /**
+   * Tests set as default links.
+   */
+  public function testDomainAppearanceSettings() {
+    $this->drupalLogin($this->adminUser);
+    $this->drupalGet('/admin/appearance');
+    $page = $this->getSession()->getPage();
+
+    // Test our form.
+    $page->findField('domain');
+    $page->findField('language');
+    // Select domain.
+    $page->selectFieldOption('domain', 'one_example_com');
+    $this->assertWaitOnAjaxRequest();
+
+    // Check if href contains correct domain_config_ui_domain param.
+    $this->assertSession()->elementAttributeContains('xpath', "//*[@id=\"system-themes-page\"]//a[contains(text(),'Set as default')]", 'href', 'domain_config_ui_domain=one_example_com');
+  }
+
+}
