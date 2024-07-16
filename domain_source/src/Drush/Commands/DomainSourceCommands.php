5c32bc5e5c5183a0c05da2750d4d814274773b05
Ide Braakman <ide@ezcompany.nl>
2024-07-16 17:06:38 +0200

    Issue #3460688: Fix phpstan issues in submodule drush commands

--- domain_source/src/Drush/Commands/DomainSourceCommands.php
+++ domain_source/src/Drush/Commands/DomainSourceCommands.php
@@ -1,22 +1,22 @@
 <?php
 
-namespace Drupal\domain_access\Commands;
+namespace Drupal\domain_source\Drush\Commands;
 
 use Consolidation\AnnotatedCommand\AnnotationData;
 use Consolidation\AnnotatedCommand\CommandData;
-use Drupal\domain\Commands\DomainCommands;
-use Drupal\domain_access\DomainAccessManagerInterface;
+use Drupal\domain\Drush\Commands\DomainCommands;
+use Drupal\domain_source\DomainSourceElementManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 
 /**
- * Drush commands for the domain access module.
+ * Drush commands for the domain source module.
  *
  * These commands mainly extend base Domain commands. See the documentation at
  * https://github.com/consolidation/annotated-command for details.
  */
-class DomainAccessCommands extends DomainCommands {
+class DomainSourceCommands extends DomainCommands {
 
   /**
    * Registers additional information to domain:info.
@@ -26,7 +26,7 @@ class DomainAccessCommands extends DomainCommands {
   public function initDomainInfo(InputInterface $input, AnnotationData $annotationData) {
     // To add a field label, append to the 'field-labels' item.
     // @todo watch https://github.com/consolidation/annotated-command/pull/174
-    $annotationData['field-labels'] .= "\n" . 'domain_access_entities: Domain access entities';
+    $annotationData['field-labels'] .= "\n" . 'domain_source_entities: Domain source entities';
   }
 
   /**
@@ -36,34 +36,35 @@ class DomainAccessCommands extends DomainCommands {
    */
   public function alterDomainInfo($result, CommandData $commandData) {
     // Display which entities are enabled for domain by checking for the fields.
-    $result['domain_access_entities'] = $this->getFieldEntities(DomainAccessManagerInterface::DOMAIN_ACCESS_FIELD);
+    $result['domain_source_entities'] = $this->getFieldEntities(DomainSourceElementManagerInterface::DOMAIN_SOURCE_FIELD);
 
     return $result;
   }
 
   /**
-   * On a domain delete, prompt for entity reassignment.
+   * On domain delete, prompt for reassign options.
    *
    * @hook option domain:delete
    */
   public function deleteOptions(Command $command, AnnotationData $annotationData) {
     $command->addOption(
-        'content-assign',
+        'source-assign',
         '',
         InputOption::VALUE_OPTIONAL,
-        'Reassign content for Domain Access',
+        'Reassign content for Domain Source',
         NULL
     );
   }
 
   /**
-   * On a domain delete, reassign entities.
+   * On domain delete, reassign entities.
    *
    * @hook on-event domain-delete
    */
-  public function domainAccessDomainDelete($target_domain, $options) {
+  public function domainSourceDomainDelete($target_domain, $options) {
     // Run our own deletion routine here.
-    if (is_null($options['content-assign'])) {
+    // @phpstan-ignore-next-line
+    if (empty($options['content-assign'])) {
       $policy_content = 'prompt';
     }
     else {
@@ -75,7 +76,7 @@ class DomainAccessCommands extends DomainCommands {
     $delete_options = [
       'entity_filter' => 'node',
       'policy' => $policy_content ?? 'ignore',
-      'field' => DomainAccessManagerInterface::DOMAIN_ACCESS_FIELD,
+      'field' => DomainSourceElementManagerInterface::DOMAIN_SOURCE_FIELD,
     ];
 
     return $this->doReassign($target_domain, $delete_options);
